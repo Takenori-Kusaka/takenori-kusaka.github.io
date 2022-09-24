780f9d3cd5b45a39c835d9755e8c1123ee616feb
Takenori Kusaka <kokorokagami@gmail.com>
2022-09-24 21:24:23 +0900

    bug fix

--- src/whisper.ps1
+++ src/whisper.ps1
@@ -1,11 +1,14 @@
 echo 'Start to convert to wav from m4a'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-ffmpeg -i ./audio/$Args[0].m4a ./audio/$Args[0].wav
+$TargetNameRaw = (Get-ChildItem . -File -Recurse -Include *.m4a | Sort-Object name -Descending)[0].BaseName
+$TargetName = $TargetNameRaw.ReplaceLineEndings("")
+$TargetName
+ffmpeg -i ./audio/$TargetName.m4a ./audio/$TargetName.wav
 echo 'Start to convert wav to text with whisper'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-whisper ./audio/$Args[0].wav --language Japanese --model medium --output_dir ./_text/
+whisper ./audio/$TargetName.wav --language Japanese --model medium --output_dir ./_text/
 echo 'Delete wav file'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-Remove-Item -Path ./audio/$Args[0].wav -Force
+Remove-Item -Path ./audio/$TargetName.wav -Force
 echo 'Complete'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
