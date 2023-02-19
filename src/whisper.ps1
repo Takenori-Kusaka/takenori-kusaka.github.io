f62239ec56eac78a9adbd8cd75853e6ddf4f8e05
Takenori Kusaka <kokorokagami@gmail.com>
2023-02-19 09:45:05 +0900

    Update whisper.ps1

--- src/whisper.ps1
+++ src/whisper.ps1
@@ -5,6 +5,8 @@ $TargetNameRaw
 $TargetName = $TargetNameRaw.ReplaceLineEndings("")
 $TargetName
 ffmpeg -i ./audio/$TargetName.m4a ./audio/$TargetName.wav
+echo 'Start to upgrade newest whisper'
+pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
 echo 'Start to convert wav to text with whisper'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
 whisper ./audio/$TargetName.wav --language Japanese --model large --device cuda --output_dir ./text/
@@ -12,5 +14,8 @@ echo 'Delete wav file'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
 Remove-Item -Path ./audio/$TargetName.wav -Force
 Remove-Item -Path ./text/$TargetName.wav.txt -Force
+Remove-Item -Path ./text/$TargetName.wav.json -Force
+Remove-Item -Path ./text/$TargetName.wav.vtt -Force
+Remove-Item -Path ./text/$TargetName.wav.srt -Force
 echo 'Complete'
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
